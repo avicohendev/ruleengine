911772213a25d779e646964677f81a063b5b8272
avicohendev <coavr00@gmail.com>
2022-01-15 18:04:40 +0200

    error fix

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,12 +2,7 @@
 import {  Sequelize } from 'sequelize'
 import * as dotenv from 'dotenv';
 dotenv.config();
-/*
-DATABASE_SERVER = metisdb1.database.windows.net
-DATABASE_NAME = ORM
-DATABASE_USER = user1
-DATABASE_PASSWORD = Trustno1
-*/
+
 const database = process.env.DATABASE_NAME!
 const user = process.env.DATABASE_USER!
 const server = process.env.DATABASE_SERVER!
