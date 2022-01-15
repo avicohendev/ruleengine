303d0af943834022673a9de003583df744a11d6d
avicohendev <coavr00@gmail.com>
2022-01-15 18:22:58 +0200

    fix database config typo

--- src/db/config.ts
+++ src/db/config.ts
@@ -7,9 +7,9 @@ const database = process.env.DATABASE_NAME!
 const user = process.env.DATABASE_USER!
 const server = process.env.DATABASE_SERVER!
 const dialect = "mssql"
-const passwird = process.env.DATABASE_PASSWORD
+const password = process.env.DATABASE_PASSWORD!
 
-const connection = new Sequelize(database, user, passwird, {
+const connection = new Sequelize(database, user, password, {
 
   host: server,
   dialect: dialect,
